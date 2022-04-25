57265eacf3cddb7d06ed7a946d6b35707b015985
TheWisePigeon <zozozozeph@gmail.com>
2022-04-25 21:53:31 +0000

    Update Hero.jsx
    made it mobile friendly

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 export default function Hero() {
     return (
         <>
-            <div className="  text-white lg:flex lg:justify-between lg:px-8 py-8 " >
-                <div className=" text-center w-3/5 lg:flex-col lg:items-center lg:justify-center">
+            <div className="  text-white lg:flex lg:justify-between lg:px-8 py-8 w-full " >
+                <div className=" text-center w-3/5 lg:flex-col lg:items-center lg:justify-center m-auto ">
                     <p className=" font-extrabold text-4xl ">Hi, I'm Joseph</p>
                     <p className=" font-extrabold text-4xl ">Fullstack developer </p>
                     <div className=" flex-col pt-4  ">
@@ -16,7 +16,7 @@ export default function Hero() {
                     
                     
                 </div>
-                <div className=" flex w-2/5 justify-center" >
+                <div className=" flex w-2/5 justify-center m-auto " >
                     <Image src="/me.jpg" alt="a picture of myself" width="300" height="300" />
                 </div>
             </div>
