2b7352447db6f36dff4987e1fdc2cf3de5e57fcb
TheWisePigeon <zozozozeph@gmail.com>
2022-05-28 17:31:29 +0000

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,15 @@
 export default function Home() {
     return (
         <>
-            <div className=" flex items-center justify-center h-screen bg-black " >
-                <div className=" flex-col">
-                    This portfolio is obsolete. Check the new one <br />
-                    <p>====</p><a className=" animate-pulse underline text-2xl text-red-400" href="http://pigeondev.vercel.app">Here</a><p>====</p>
-                </div>
+            <div className=" flex  justify-between h-screen bg-black p-60" >
+                    <div className=" ">
+                        This portfolio is obsolete. Check the new one <br />
+                        <p>====</p><a className=" animate-pulse underline text-2xl text-red-400" href="http://pigeondev.vercel.app">Here</a><p>====</p>
+                    </div>
+                    <div >
+                        Ce portfolio est obsolète. Veuillez trouver la nouvelle version <br />
+                        <p>====</p><a className=" animate-pulse underline text-2xl text-red-400" href="http://pigeondev.vercel.app">ICI</a><p>====</p>
+                    </div>
             </div>
         </>
     )
